8ee1bd33662ad312db8273cbd287b78b86a3f74f
Mason Barnes <hypnosismcb@yahoo.com>
2020-11-05 11:57:21 -0800

    Added functionality for user to view product details

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   render() {
-    return (
-      <div>
-        <Header/>
-        <ProductList/>
-      </div>
-    );
+    if (this.state.view.name === 'catalog') {
+      return (
+        <div>
+          <Header />
+          <ProductList setView={this.setView} />
+        </div>
+      );
+    } else if (this.state.view.name === 'details') {
+      return (
+        <div>
+          <Header/>
+          <ProductDetails setView={this.setView} productId={this.state.view.params} />
+        </div>
+      );
+    }
+
   }
 }
